ff3e8076b7a72db2948c29daa7223641ede1ba41
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-09-22 16:14:04 +0900

    added map

--- react-flats/src/components/app.jsx
+++ react-flats/src/components/app.jsx
@@ -1,10 +1,42 @@
 import React, { Component } from "react";
+import flats from "../../data/flats";
+import FlatList from "./flatList";
+import GoogleMapReact from 'google-map-react';
+import Marker from "./marker";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFlat: flats[0],
+      flats
+    };
+  }
+
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
+  }
+
   render() {
     return (
       <div>
-        <h1>left</h1>
+        <FlatList 
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
+        />
+        <div className="map-container">
+          <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
+            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+          </GoogleMapReact>
+        </div>
       </div>
     );
   }
---
+++ react-flats/src/components/flat.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class Flat extends Component {
+  handleClick = () => {
+    const {selectFlat, index } = this.props;
+    selectFlat(index);
+  }
+
+  render () {
+    const { selected, flat } = this.props;
+    return (
+      <div className={`card${selected ? ' active' : ''}`} style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('${flat.imageUrl}')` }}>
+        <div className="card-category">
+          {flat.price}
+          {flat.priceCurrency}
+        </div>
+        <div className="card-description">
+          <h2>{flat.name}</h2>
+        </div>
+        <a href="#" className="card-link" onClick={this.handleClick}></a>
+      </div>
+    );
+  }
+}
+
+export default Flat;
